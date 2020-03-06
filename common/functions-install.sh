9514ce7d8fee8ad790b80a1e2b4abaafceca5a56
GongT <admin@gongt.me>
2020-03-06 12:38:48 +0800

    split common folder

--- common/functions-install.sh
+++
@@ -1,7 +0,0 @@
-source "$(dirname "$(realpath "${BASH_SOURCE[0]}")")/functions.sh"
-
-source "$COMMON_LIB_ROOT/functions/networking.sh"
-source "$COMMON_LIB_ROOT/functions/environments.sh"
-source "$COMMON_LIB_ROOT/functions/systemd.sh"
-source "$COMMON_LIB_ROOT/functions/install.sh"
-
--- common/functions/output.sh
+++
@@ -1,6 +0,0 @@
-function die() {
-    echo "$@" >&2 ; exit 1
-}
-function info() {
-    echo -e "\e[38;5;14m$*\e[0m"
-}
