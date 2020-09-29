32167b843c74a42c7a1a9062f68f524e9ae2100b
GongT <admin@gongt.me>
2020-09-30 02:15:17 +0800

    fix build

--- virtual-gateway/build.sh
+++ virtual-gateway/build.sh
@@ -27,7 +27,7 @@ download_wireguard() {
 
 	install -D --verbose --compare --mode=0755 --no-target-directory "$DOWNLOADED" "$MNT/usr/libexec/wireguard-config-client"
 
-	VERSION=$(xbuildah run sh -c '/usr/libexec/wireguard-config-client -V')
+	VERSION=$(xbuildah run "$RESULT" sh -c '/usr/libexec/wireguard-config-client -V')
 	info "VERSION = $VERSION"
 
 	buildah config --label "client-version=$VERSION" "$RESULT"
