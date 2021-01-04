5650419e4a0c7484f9c9d707b0bd95e6a15b9993
GongT <admin@gongt.me>
2021-01-04 15:59:12 +0800

    [bttraker] change domain name to bittorrent.

--- bttracker/install-service.sh
+++ bttracker/install-service.sh
@@ -8,8 +8,9 @@ source ../common/functions-install.sh
 create_pod_service_unit gongt/bttracker
 unit_unit Description bttracker
 unit_start_notify output "Ok, All setup"
-network_use_gateway
+network_use_auto
 # unit_body Restart always
+# unit_podman_image_pull never
 unit_fs_bind share/nginx /run/nginx
 shared_sockets_provide bittorrent-tracker
 unit_finish
