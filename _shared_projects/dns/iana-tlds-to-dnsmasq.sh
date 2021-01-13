eaa512465f743dfda4bccc5e510e71d1c2dd7548
GongT <admin@gongt.me>
2021-01-13 18:04:40 +0800

    fix create file

--- _shared_projects/dns/iana-tlds-to-dnsmasq.sh
+++ _shared_projects/dns/iana-tlds-to-dnsmasq.sh
@@ -6,8 +6,15 @@ tlds_etag() {
 	http_get_etag "$TLDS_URL"
 }
 tlds_update() {
-	local FILE LINE LINES MNT
+	local FILE LINE LINES MNT F D
 	MNT=$(buildah mount "$1")
+	F="$MNT${SAVE_TO:-/etc/dnsmasq.d/iana.conf}"
+	D=$(dirname "$F")
+
+	if ! [[ -d $D ]]; then
+		mkdir "$D"
+	fi
+
 	FILE=$(download_file_force "$TLDS_URL" tlds.txt)
 	mapfile -t LINES <"$FILE"
 	for LINE in "${LINES[@]}"; do
@@ -16,6 +23,6 @@ tlds_update() {
 		fi
 		echo "address=/${LINE,,}/::"
 		echo "server=/${LINE,,}/127.0.0.1#5353"
-	done >"$MNT${SAVE_TO:-/etc/dnsmasq.d/iana.conf}"
+	done >"$F"
 }
 buildah_cache2 "$1" tlds_etag tlds_update
