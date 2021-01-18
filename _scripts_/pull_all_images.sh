1a11824d761bdc94f1f59ffe73b3d19f20d00781
GongT <admin@gongt.me>
2021-01-18 16:12:57 +0800

    update script

--- _scripts_/pull_all_images.sh
+++ _scripts_/pull_all_images.sh
@@ -7,7 +7,8 @@ cd "$(dirname "$(realpath "${BASH_SOURCE[0]}")")" || die "???"
 export SKIP_REMOVE=yes
 mapfile -t IMAGES < <(podman images | grep gongt/ | grep latest | awk '{print $1}')
 
+SKIP_REMOVE=yes
 for IMAGE in "${IMAGES[@]}"; do
-	echo "* pull image $IMAGE..."
+	echo -e "\e[38;5;10m * pull image $IMAGE...\e[0m"
 	bash ../common/tools/pull-image.sh "$IMAGE" always
 done
