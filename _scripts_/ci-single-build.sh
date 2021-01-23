d0d17d9eca90153ccd9833b71ab8cae0c916b836
GongT <admin@gongt.me>
2021-01-24 05:25:10 +0800

    add skipEarlyDetect

--- _scripts_/ci-single-build.sh
+++ _scripts_/ci-single-build.sh
@@ -6,7 +6,7 @@ cd "$(dirname "$(realpath "${BASH_SOURCE[0]}")")/.."
 # shellcheck source=../common/functions-build-host.sh
 source "./common/functions-build-host.sh"
 
-if [[ "${CI+found}" != found ]]; then
+if [[ ${CI+found} != found ]]; then
 	die "This script is only for CI"
 fi
 
@@ -19,11 +19,11 @@ HASH=$(hash_current_folder)
 
 EXISTS="$(get_up_to_date_image "$HASH")"
 
-if [[ "$EXISTS" ]]; then
+if [[ "$EXISTS" ]] && [[ ! ${TRIGGER_SKIP_EARLY_DETECT:-} ]]; then
 	info "$NAME - $HASH - exists [$EXISTS]"
 	control_ci "set-env" "LAST_COMMITED_IMAGE" "$EXISTS"
 else
 	bash "./build.sh" || die "Build failed"
 fi
 
-podman rmi "localhost/gongt/${NAME}" &> /dev/null || true
+podman rmi "localhost/gongt/${NAME}" &>/dev/null || true
