4f0ca21898c05d017ba9d74195d74ed806d606cd
GongT <admin@gongt.me>
2021-01-29 16:42:00 +0800

    normal update

--- _scripts_/retry.sh
+++ _scripts_/retry.sh
@@ -18,4 +18,17 @@ for I in $(seq 1 3); do
 	fi
 done
 
+echo "::: Failed with proxy, retry without proxy :::" >&2
+
+export http_proxy='' https_proxy='' all_proxy='' HTTP_PROXY='' HTTPS_PROXY='' ALL_PROXY=''
+for I in $(seq 1 3); do
+	echo "============================== try $I ==============================" >&2
+	"$@"
+	RET=$?
+	echo "============================== try $I exit $RET ====================" >&2
+	if [[ $RET -eq 0 ]]; then
+		exit 0
+	fi
+done
+
 exit $RET
