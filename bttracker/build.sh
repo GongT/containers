9c9bd0bd1e2a88cc3a172e59362736dd82d4f720
GongT <admin@gongt.me>
2021-07-01 18:37:28 +0800

    [bttracker] fix build

--- bttracker/build.sh
+++ bttracker/build.sh
@@ -5,19 +5,17 @@ set -Eeuo pipefail
 cd "$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
 source ../common/functions-build.sh
 
-info "starting..."
-RESULT=$(create_if_not bttracker-worker node:alpine)
+BUILDAH_LAST_IMAGE="node:alpine"
 
-info "pnpm install..."
-pnpm -C app --prod install
+STEP="复制应用文件并安装依赖"
+mkdir -p "$SYSTEM_COMMON_CACHE/npm"
+merge_local_fs "bttracker" "--volume=$SYSTEM_COMMON_CACHE/npm:/root/.npm" scripts/npm-install.sh
 
-buildah copy "$RESULT" app /data/app
-info "copy files..."
-
-buildah config --entrypoint '' --cmd 'node --unhandled-rejections=strict /data/app/lib/main.js' "$RESULT"
-buildah config --stop-signal SIGINT --volume /data/store "$RESULT"
-buildah config --author "GongT <admin@gongt.me>" --created-by "#MAGIC!" --label name=gongt/bttracker "$RESULT"
+buildah_config "bttracker" --entrypoint '' --cmd 'node --unhandled-rejections=strict /data/app/lib/main.js' \
+	--stop-signal SIGINT --volume /data/store \
+	--author "GongT <admin@gongt.me>" --created-by "#MAGIC!"
 info "settings updated..."
 
+RESULT=$(create_if_not "bttracker" "$BUILDAH_LAST_IMAGE")
 buildah commit "$RESULT" gongt/bttracker
 info "Done!"
