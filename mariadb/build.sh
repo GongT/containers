5bda623f0eebbbad039aaccdd1c6ce0609b49806
GongT <admin@gongt.me>
2021-07-03 00:16:18 +0800

    [mariadb] build with cache

--- mariadb/build.sh
+++ mariadb/build.sh
@@ -5,20 +5,18 @@ set -Eeuo pipefail
 cd "$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
 source ../common/functions-build.sh
 
-info "starting..."
+### 依赖
+DEPS=(mariadb mariadb-client bash phpmyadmin php-fpm nginx p7zip)
+make_base_image_by_apk "gongt/alpine-init" "mariadb" "${DEPS[@]}" <scripts/build-script.sh
 RESULT=$(create_if_not mariadb-worker gongt/alpine-init)
+### 依赖 END
 
-info "installing..."
-cat scripts/build-script.sh | buildah run $(use_alpine_apk_cache) "$RESULT" -- sh
-info "install complete..."
+merge_local_fs "mariadb"
 
-buildah copy "$RESULT" fs /
+buildah_config "mariadb" --cmd '/sbin/init' \
+	--volume /var/lib/mysql --volume /var/log --port 3306 --stop-signal SIGINT \
+	--author "GongT <admin@gongt.me>" --created-by "#MAGIC!" --label name=gongt/mariadb
 
-buildah config "$RESULT"
-buildah config --cmd '/sbin/init' "$RESULT"
-buildah config --volume /var/lib/mysql --volume /var/log --port 3306 --stop-signal SIGINT "$RESULT"
-buildah config --author "GongT <admin@gongt.me>" --created-by "#MAGIC!" --label name=gongt/mariadb "$RESULT"
-info "settings updated..."
-
+RESULT=$(create_if_not "mariadb" "$BUILDAH_LAST_IMAGE")
 buildah commit "$RESULT" gongt/mariadb
 info "Done!"
--- mariadb/scripts/build-script.sh
+++ mariadb/scripts/build-script.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 
-apk add -U mariadb mariadb-client bash phpmyadmin php-fpm nginx p7zip
 rm -rf /etc/nginx
 mkdir -p /var/lib/nginx/logs /run/nginx
