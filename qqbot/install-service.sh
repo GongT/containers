7de913e7780eb11b283be6e51ad7c0c080298a93
GongT <admin@gongt.me>
2021-08-25 20:32:07 +0800

    [qqbot] listen 80 in container

--- qqbot/install-service.sh
+++ qqbot/install-service.sh
@@ -17,7 +17,7 @@ unit_podman_arguments -it
 unit_body Restart no
 unit_start_notify output "Login successful"
 
-network_use_bridge 56084/tcp
+network_use_bridge 56080:80/tcp # this for debug only
 
 unit_fs_bind config/qqbot /mirai/config
 unit_fs_bind data/qqbot/data /mirai/data
