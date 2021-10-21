0ba3cffe10f991b506c45662b6da7e89cd22e60d
GongT <admin@gongt.me>
2021-10-21 21:21:59 +0800

    [liverecord] remove mp4 folder

--- liverecord/install-service.sh
+++ liverecord/install-service.sh
@@ -17,7 +17,6 @@ systemd_slice_type normal
 unit_body Restart no
 # unit_podman_image_pull never
 unit_fs_bind "/data/Volumes/VideoRecord/bilibili/%i/raw" /data/raw
-unit_fs_bind "/data/Volumes/VideoRecord/bilibili/%i/mp4" /data/mp4
 unit_finish
 
 systemctl enable "liverecord.pod@$LIVE_ROOM.service"
