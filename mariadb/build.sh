a075b86c833f3763646d7a1afb7d21504b2493b9
GongT <admin@gongt.me>
2022-01-18 04:44:35 +0800

    [mysql] add logrotate

--- mariadb/build.sh
+++ mariadb/build.sh
@@ -6,7 +6,7 @@ cd "$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
 source ../common/functions-build.sh
 
 ### 依赖
-DEPS=(mariadb mariadb-client mariadb-backup bash phpmyadmin php-fpm nginx p7zip)
+DEPS=(mariadb mariadb-client mariadb-backup bash phpmyadmin php-fpm nginx p7zip logrotate)
 make_base_image_by_apk "gongt/alpine-init" "mariadb" "${DEPS[@]}" <scripts/build-script.sh
 RESULT=$(create_if_not mariadb-worker gongt/alpine-init)
 ### 依赖 END
