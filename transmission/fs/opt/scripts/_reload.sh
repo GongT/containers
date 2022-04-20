d4078c82359f7eda5e0c19ecfd5efffc0caa9dae
GongT <admin@gongt.me>
2022-04-20 23:34:52 +0800

    [common] sync

--- transmission/fs/opt/scripts/_reload.sh
+++ transmission/fs/opt/scripts/_reload.sh
@@ -6,9 +6,9 @@ T="/run/nginx/vhost.d/transmission.conf"
 cp -v "/opt/scripts/nginx.conf" "$T"
 curl --unix /run/sockets/nginx.reload.sock http://_/
 
-if ! [[ -f /opt/config/config.json ]];then
-	touch /opt/config/config.json
+if ! [[ -f /opt/config/config.json ]]; then
+	echo "{}" >/opt/config/config.json
 fi
 
 rm -f /opt/data/settings.json
-jq -s '.[0] * .[1]' /opt/scripts/config.json /opt/config/config.json > /opt/data/settings.json
+jq -s '.[0] * .[1]' /opt/scripts/config.json /opt/config/config.json >/opt/data/settings.json
