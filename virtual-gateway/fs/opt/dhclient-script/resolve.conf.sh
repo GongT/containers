53b1194a5bcfc19e5ce162d7b534c814df8a7713
GongT <admin@gongt.me>
2022-08-07 22:40:13 +0800

    [virtual-gateway] fix resolve order

--- virtual-gateway/fs/opt/dhclient-script/resolve.conf.sh
+++ virtual-gateway/fs/opt/dhclient-script/resolve.conf.sh
@@ -4,21 +4,6 @@ set -Eeuo pipefail
 
 mkdir -p /etc/resolv.conf.d
 
-{
-	if [[ "${RESOLVE_OPTIONS:-}" ]]; then
-		echo "options $RESOLVE_OPTIONS"
-	fi
-	if [[ "${RESOLVE_SEARCH:-}" ]]; then
-		echo "options $RESOLVE_SEARCH"
-	fi
-	if [[ "${NSS:-}" ]]; then
-		mapfile -d ' ' -t NSS < <(echo "$NSS")
-		for NS in "${NSS[@]}"; do
-			echo "nameserver $NS"
-		done
-	fi
-} >/etc/resolv.conf
-
 NAME="/etc/resolv.conf.d/$1"
 shift
 
@@ -26,9 +11,26 @@ rm -f "$NAME"
 for I in "$@"; do
 	echo "nameserver $I" >>"$NAME"
 done
-
-for I in /etc/resolv.conf.d/*; do
-	cat "$I" >>/etc/resolv.conf
-done
-
 echo "rewrite: resolv.conf (add $NAME)" >&2
+
+{
+	if [[ "${RESOLVE_OPTIONS:-}" ]]; then
+		echo "# RESOLVE_OPTIONS"
+		echo "options $RESOLVE_OPTIONS"
+	fi
+	if [[ "${RESOLVE_SEARCH:-}" ]]; then
+		echo "# RESOLVE_SEARCH"
+		echo "options $RESOLVE_SEARCH"
+	fi
+	for I in /etc/resolv.conf.d/*; do
+		echo "# content from $I"
+		cat "$I"
+	done
+	if [[ "${NSS:-}" ]]; then
+		echo "# NSS"
+		mapfile -d ' ' -t NSS < <(echo "$NSS")
+		for NS in "${NSS[@]}"; do
+			echo "nameserver $NS"
+		done
+	fi
+} >/etc/resolv.conf
