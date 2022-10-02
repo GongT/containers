c2eab00e242fd14534b3dd950902f05e7c79d178
GongT <admin@gongt.me>
2022-10-02 16:03:52 +0800

    [nginx] fix docker restart

--- nginx/fs/usr/sbin/nginx.sh
+++ nginx/fs/usr/sbin/nginx.sh
@@ -86,8 +86,10 @@ else
 fi
 
 if [[ -e /config/dhparam.pem ]]; then
-	echo "ssl_dhparam /config/dhparam.pem;" >>/etc/nginx/params/ssl_params
-	echo "ssl_dhparam /config/dhparam.pem;" >>/etc/nginx/params/ssl_params_stream
+	if ! grep -v 'ssl_dhparam ' /etc/nginx/params/ssl_params; then
+		echo "ssl_dhparam /config/dhparam.pem;" >>/etc/nginx/params/ssl_params
+		echo "ssl_dhparam /config/dhparam.pem;" >>/etc/nginx/params/ssl_params_stream
+	fi
 elif ! [[ $DISABLE_SSL ]]; then
 	echo 'Not using DH parameters file! generate using "openssl dhparam -dsaparam -out /XXX/config/nginx/dhparam.pem 4096"' >&2
 fi
