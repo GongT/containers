ed94996fa0a396c1c9272228ccab67cace8682ac
GongT <admin@gongt.me>
2022-10-15 19:25:17 +0800

    [factorio] fix busybox

--- factorio/install-service.sh
+++ factorio/install-service.sh
@@ -10,27 +10,22 @@ DIST_TAG=stable
 
 arg_string + SERVER_TITLE title "server title"
 arg_string SERVER_DESCRIPTION desc "server description"
-arg_string + USERNAME u "factorio online game username"
-arg_string + PASSWORD p "factorio online game password"
+arg_string + USERNAME u "factorio user username"
+arg_string + PASSWORD p "factorio user password"
 arg_string GAME_PASSWORD password "server join password"
 arg_string DIST_TAG dist-tag "version (stable or latest)"
-arg_string PORT p/port "server port"
+arg_string PORT port "server port"
 arg_finish "$@"
 
 declare -i RCON_PORT="$PORT - 34197 + 27015"
 
-create_pod_service_unit factroio
+create_pod_service_unit factorio
 unit_podman_image gongt/factorio
 unit_unit Description factorio server
 unit_data danger
 unit_body TimeoutStartSec 300s
 unit_body RestartPreventExitStatus 66
 unit_start_notify output "Obtained serverPadlock for serverHash"
-unit_podman_arguments \
-	"--env=DIST_TAG=$DIST_TAG" \
-	"--env=SERVER_PORT=$PORT" \
-	"--env=RCON_PORT=$RCON_PORT" \
-	"--env=PROXY_SERVER=proxy-server.:3271"
 
 network_use_auto "$PORT/udp" "$RCON_PORT/tcp"
 systemd_slice_type entertainment
@@ -45,6 +40,10 @@ unit_podman_safe_environment \
 	"SERVER_DESCRIPTION=$SERVER_DESCRIPTION" \
 	"USERNAME=$USERNAME" \
 	"PASSWORD=$PASSWORD" \
-	"GAME_PASSWORD=${GAME_PASSWORD:-}"
+	"GAME_PASSWORD=${GAME_PASSWORD:-}" \
+	"DIST_TAG=$DIST_TAG" \
+	"SERVER_PORT=$PORT" \
+	"RCON_PORT=$RCON_PORT" \
+	"PROXY_SERVER=proxy-server.:3271"
 
 unit_finish
