9e66ee9b25c67d5379d871e3a07823a257e8b4c2
GongT <admin@gongt.me>
2022-12-29 08:00:43 +0800

    [gamedisk/qbittorrent] update dhcp

--- gamedisk/source/post-copy.sh
+++ gamedisk/source/post-copy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 set -Eeuo pipefail
- 
-systemctl enable tgtd.service dhclient.service dhclient6.service iperf3-server@33233.service
+
+systemctl enable tgtd.service dhclient.service iperf3-server@33233.service
 rm -f /etc/tgt/conf.d/sample.conf
