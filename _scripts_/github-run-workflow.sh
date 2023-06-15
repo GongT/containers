e901f3c2ffdd41b607f8bb1eba9f6282242f0e9f
GongT <admin@gongt.me>
2023-06-16 01:57:26 +0800

    [cloud] use rawhide

--- _scripts_/github-run-workflow.sh
+++ _scripts_/github-run-workflow.sh
@@ -5,5 +5,5 @@ set -Eeuo pipefail
 cd "$(dirname "$(realpath "${BASH_SOURCE[0]}")")/.."
 
 for i in .github/workflows/generated-*yaml; do
-	gh workflow run "$(basename "$i")" --ref master -f skipEarlyDetect=yes
+	gh workflow run "$(basename "$i")" --ref master -f forceDnf=true
 done
