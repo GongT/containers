826c166876696b89421f1c4b55384d2de8e7bb1a
GongT <admin@gongt.me>
2023-06-20 20:11:34 +0800

    [liverecord] update install

--- liverecord/install-service.sh
+++ liverecord/install-service.sh
@@ -14,8 +14,8 @@ unit_start_notify output '弹幕服务器已连接'
 systemd_slice_type normal
 environment_variable "LIVE_ROOMS=$LIVE_ROOMS"
 
-# unit_body Restart on-failure
-unit_podman_image_pull never
+unit_body Restart on-failure
+# unit_podman_image_pull never
 
 network_use_bridge
 unit_fs_bind share/nginx /run/nginx
