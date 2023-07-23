49f07214f88bde85ff54af22c36f77644eecbe99
GongT <admin@gongt.me>
2023-07-23 20:58:03 +0800

    [samba] add 100 group

--- samba/scripts/prepare.sh
+++ samba/scripts/prepare.sh
@@ -6,6 +6,11 @@ if ! grep -q -- smbusers /etc/group; then
 	groupadd smbusers
 fi
 
+if ! grep -q -- '100' /etc/passwd; then
+	# groupadd --gid 100 users
+	groupmod --gid 100 users
+fi
+
 if ! grep -q -- media_rw /etc/passwd; then
 	useradd --groups users,root --uid 100 --gid 100 --no-user-group --no-create-home media_rw
 fi
