38509dc45fae0fac0f2b9811b28eceb80b096fde
GongT <admin@gongt.me>
2024-09-14 03:43:26 +0800

    secrets

--- _scripts_/prepare-secrets.sh
+++ _scripts_/prepare-secrets.sh
@@ -15,7 +15,7 @@ if [[ "${CI-}" ]]; then
 	sudo apt install jq gnupg podman buildah mount
 fi
 
-sudo cp "../90-myregistry.conf" /etc/containers/registries.conf.d/
+# sudo cp "./_scripts_/80-myregistry.conf" /etc/containers/registries.conf.d/
 
 JSON=$(gpg --quiet --batch --yes --passphrase "$SECRET_PASSWORD" --decrypt build-secrets.json.gpg)
 
