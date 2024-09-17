ed38fcab89a506551528b9c9b89af13280df8580
GongT <admin@gongt.me>
2024-09-17 23:49:53 +0800

    [mariadb] fix order

--- mariadb/build.sh
+++ mariadb/build.sh
@@ -14,11 +14,11 @@ dnf_use_environment
 dnf_install_step "mariadb" scripts/deps.lst scripts/clean-install.sh
 ### 依赖项目 END
 
+merge_local_fs "mariadb"
+
 setup_systemd "mariadb" \
 	enable "UNITS=mariadb.service logrotate.timer nginx.service php-fpm.service backup.timer" \
 	nginx_attach "NGINX_CONFIG=/opt/phpmyadmin.conf"
-
-merge_local_fs "mariadb"
 
 buildah_config "mariadb" \
 	--volume /var/lib/mysql --volume /var/log --port 3306 --stop-signal SIGINT \
