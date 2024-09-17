15261a6ced75513e0fdd5970d7680269cdf3a40d
GongT <admin@gongt.me>
2024-09-18 03:01:34 +0800

    [cloud] upgrade

--- cloud/build.sh
+++ cloud/build.sh
@@ -15,27 +15,15 @@ dnf_use_environment
 dnf_install_step "nextcloud" scripts/deps.lst scripts/clean-install.sh
 ### 依赖项目 END
 
-setup_systemd "nextcloud"
-
 merge_local_fs "nextcloud"
-info "copy config files complete..."
 
-STEP="安装"
-function hash_install_script() {
-	hash_path scripts/build-script.sh
-}
-function run_install_script() {
-	buildah run "--volume=$(pwd)/scripts/build-script.sh:/tmp/build-script" "$1" bash '/tmp/build-script'
-}
-buildah_cache "nextcloud" hash_install_script run_install_script
+setup_systemd "nextcloud" \
+	enable "UNITS=nginx.service php-fpm.service nextcloud-clean.timer redis.service" \
+	nginx_attach "NGINX_CONFIG="
 
-STEP="配置镜像"
 buildah_config "nextcloud" \
 	--author "GongT <admin@gongt.me>" \
 	--created-by "#MAGIC!!" \
 	--label name=gongt/cloud
 
-info "settings updated..."
-
 buildah_finalize_image "cloud" gongt/cloud
-info "Done!"
--- cloud/scripts/clean-install.sh
+++ cloud/scripts/clean-install.sh
@@ -1,9 +1,18 @@
 #!/usr/bin/env bash
 
-set -xEeuo pipefail
+set -Eeuo pipefail
 
 mkdir -p /var/log/nginx /var/log/php-fpm
 chmod 0777 /var/log/nginx /var/log/php-fpm
 
 rm -rf /etc/nginx /etc/php-fpm.d
 mkdir -p /etc/nginx /etc/php-fpm.d
+
+if id nextcloud 2>/dev/null; then
+	userdel nextcloud
+fi
+
+groupadd --force --gid 100 users
+useradd --no-create-home --home-dir /var/lib/nextcloud --shell /sbin/nologin --no-user-group --gid users --uid 100 media_rw
+
+chown 100:100 /var/lib/nextcloud -R
