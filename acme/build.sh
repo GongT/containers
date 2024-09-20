30a4128dae14ec02770ee86a50170e383144d44c
GongT <admin@gongt.me>
2024-09-20 18:48:15 +0800

    [acme] rebuild

--- acme/build.sh
+++ acme/build.sh
@@ -47,8 +47,8 @@ merge_local_fs "acme"
 
 STEP="配置镜像信息"
 buildah_config "acme" \
-	"--entrypoint=$(json_array /usr/bin/bash)" \
-	"--cmd=$(json_array /opt/entrypoint.sh)" --stop-signal=SIGINT \
+	"--entrypoint=$(json_array /opt/entrypoint.sh)" \
+	--stop-signal=SIGINT \
 	--volume /opt/data --volume /etc/ACME \
 	"--label=${LABELID_USE_NGINX_ATTACH}=yes"
 info "settings updated..."
