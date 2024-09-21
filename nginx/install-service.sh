71356c48f09f71b309a57714780d9e4832ec86e4
GongT <admin@gongt.me>
2024-09-21 19:28:21 +0800

    [nginx] fix start order

--- nginx/install-service.sh
+++ nginx/install-service.sh
@@ -45,7 +45,7 @@ unit_fs_bind /data/DevelopmentRoot /data/DevelopmentRoot
 unit_fs_bind data/nginx /data
 unit_fs_bind config/nginx /config
 unit_fs_bind logs/nginx /var/log/nginx
-unit_fs_tempfs 8M /run
+unit_fs_tempfs 64M /run
 unit_fs_tempfs 2G /tmp
 if ! [[ $DISABLE_SSL ]]; then
 	unit_fs_bind share/ssl /etc/ACME
