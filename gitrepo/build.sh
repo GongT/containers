066adae76482d6317f1f75c40d3830b98b3ca168
GongT <admin@gongt.me>
2024-09-23 17:03:57 +0800

    [gitrepo] fix name

--- gitrepo/build.sh
+++ gitrepo/build.sh
@@ -14,7 +14,7 @@ dnf_install_step "gitrepo" scripts/requirements.lst
 merge_local_fs "gitrepo"
 
 setup_systemd "gitrepo" \
-	enable "REQUIRE=fcgiwrap.socket" \
+	enable "REQUIRE=fcgiwrap.service" \
 	nginx_attach
 
 buildah_finalize_image gitrepo gongt/gitrepo
