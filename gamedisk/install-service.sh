80f500852b528e139432fdb4bb80375d0b63ca6c
GongT <admin@gongt.me>
2024-09-29 02:24:32 +0800

    [skip ci] update script

--- gamedisk/install-service.sh
+++ gamedisk/install-service.sh
@@ -9,11 +9,6 @@ auto_create_pod_service_unit
 unit_podman_image registry.gongt.me/gongt/gamedisk
 unit_unit Description "iSCSI target daemon for game disk"
 
-unit_unit After 'dev-mapper-game\x2dscsi.device'
-unit_unit Requires 'dev-mapper-game\x2dscsi.device'
-environment_variable "DISK_TO_USE=/dev/xvdb"
-podman_engine_params --device=/dev/mapper/game-scsi:/dev/xvdb:rw
-
 # unit_podman_image_pull never
 unit_data danger
 systemd_slice_type infrastructure
@@ -21,5 +16,10 @@ systemd_slice_type infrastructure
 add_network_privilege
 network_use_veth bridge0
 podman_engine_params --mac-address=3E:F4:F3:CE:1D:80
+
+unit_unit After 'dev-mapper-game\x2dscsi.device'
+unit_unit Requires 'dev-mapper-game\x2dscsi.device'
+environment_variable "DISK_TO_USE=/dev/xvdb"
+podman_engine_params --device=/dev/mapper/game-scsi:/dev/xvdb:rw
 
 unit_finish
--- qbittorrent/install-service.sh
+++ qbittorrent/install-service.sh
@@ -6,18 +6,16 @@ cd "$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
 source ../common/functions-install.sh
 
 create_pod_service_unit gongt/qbittorrent
+unit_podman_image registry.gongt.me/gongt/qbittorrent
 unit_unit Description qbittorrent
-for I in /data/Volumes/*; do
-	unit_unit RequiresMountsFor "${I}"
-done
 
-network_use_veth bridge0
-podman_engine_params --mac-address=4A:E1:A2:4E:D5:6E
-add_network_privilege
-
+# unit_body Restart always
 unit_data danger
 systemd_slice_type idle
-# unit_body Restart always
+
+add_network_privilege
+network_use_veth bridge0
+podman_engine_params --mac-address=4A:E1:A2:4E:D5:6E
 
 unit_fs_bind config/qbittorrent /opt/qBittorrent/config
 unit_fs_bind data/qbittorrent /opt/qBittorrent/data
@@ -27,5 +25,9 @@ unit_fs_bind /data/Volumes /data/Volumes
 unit_body TimeoutStartSec 2min
 podman_engine_params --env="LANG=zh_CN.utf8"
 shared_sockets_provide qbittorrent-admin
+
+for I in /data/Volumes/*; do
+	unit_unit RequiresMountsFor "${I}"
+done
 
 unit_finish
