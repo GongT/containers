cc38e157803acd60e84938fdc0da06b6b9e56d35
GongT <admin@gongt.me>
2024-12-23 05:27:02 +0800

    [qbittorrent/qqbot] update graphical builds

--- systemd-base-image/graphical/build-steps.sh
+++ systemd-base-image/graphical/build-steps.sh
@@ -1,5 +1,8 @@
+STEP="安装图形界面"
 dnf_install_step "systemd-graphical" scripts/dependencies.lst
+STEP="复制图形界面配置文件"
 merge_local_fs "systemd-graphical" scripts/prepare.sh
+STEP="配置vnc服务"
 setup_systemd "systemd-graphical" \
 	basic "DEFAULT_TARGET=graphical.target" \
 	enable "REQUIRE=novnc.service i3.service" \
