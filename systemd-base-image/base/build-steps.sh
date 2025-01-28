bccff943bd3f96529a0a5e769dbf6c2fbff422e2
GongT <admin@gongt.me>
2025-01-29 00:50:56 +0800

    [base] add locale

--- systemd-base-image/base/build-steps.sh
+++ systemd-base-image/base/build-steps.sh
@@ -1,5 +1,5 @@
 buildah_cache_start "quay.io/fedora/fedora-minimal"
 dnf_use_environment
-dnf_install_step "systemd" scripts/dependencies.lst
+dnf_install_step "systemd" scripts/dependencies.lst scripts/after-install.sh
 
 setup_systemd "systemd" basic
---
+++ systemd-base-image/base/scripts/after-install.sh
@@ -0,0 +1,5 @@
+
+echo 'LANG=zh_CN.UTF-8
+LC_TIME=en_DK.UTF-8
+LC_MEASUREMENT=metric
+' >/etc/locale.conf
