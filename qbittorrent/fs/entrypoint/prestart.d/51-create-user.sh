40e7f0a933deac34b8f5fffaaffcd8a467f1ba0f
GongT <admin@gongt.me>
2025-08-07 13:18:47 +0800

    [qbittorrent] fix conflict env

--- qbittorrent/fs/entrypoint/prestart.d/51-create-user.sh
+++ qbittorrent/fs/entrypoint/prestart.d/51-create-user.sh
@@ -1,22 +1,22 @@
-UNAME=${USER_NAME:-}
-UID=${USER_ID:-}
-GID=${GROUP_ID:-}
+U_NAME=${USER_NAME:-}
+U_ID=${USER_ID:-}
+G_ID=${GROUP_ID:-}
 
-if [[ ! "${UNAME}" ]]; then
-	UNAME=media_rw
-	UID=100
-	GID=100
+if [[ ! "${U_NAME}" ]]; then
+	U_NAME=media_rw
+	U_ID=100
+	G_ID=100
 fi
 
 unset USER_NAME USER_ID GROUP_ID
 
-exportenv USER_NAME "${UNAME}"
-system_ensure_group "$GID" users
-system_ensure_user "$UID" "$UNAME" "$GID"
+exportenv USER_NAME "${U_NAME}"
+system_ensure_group "$G_ID" users
+system_ensure_user "$U_ID" "$U_NAME" "$G_ID"
 
-mkdir -p "/home/${UNAME}"
+mkdir -p "/home/${U_NAME}"
 
-echo "User=${UNAME}" >> /usr/lib/systemd/system/qbittorrent.service
+echo "User=${U_NAME}" >> /usr/lib/systemd/system/qbittorrent.service
 
 if ! [[ -e /opt/qBittorrent/config/qBittorrent.conf ]]; then
 	log "first run."
@@ -24,6 +24,6 @@ if ! [[ -e /opt/qBittorrent/config/qBittorrent.conf ]]; then
 	cp /opt/scripts/qBittorrent.conf /opt/qBittorrent/config/qBittorrent.conf
 fi
 
-chown -R "${UNAME}:users" "/home/${UNAME}" /opt/qBittorrent
+chown -R "${U_NAME}:users" "/home/${U_NAME}" /opt/qBittorrent
 
-unset UNAME UID GID
+unset U_NAME U_ID G_ID
